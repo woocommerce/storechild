1e9c2520953ad07c5a33cbf86bf7cfbe3e88e7c8
James Koster <james@jameskoster.co.uk>
2016-01-19 09:54:17 +0000

    Include an example of how to correctly add CSS inline, pulled from Customizer settings

--- inc/customizer/hooks.php
+++ inc/customizer/hooks.php
@@ -6,6 +6,11 @@
  */
 
 /**
+ * Add Storechild specific CSS selectors based on customizer settings
+ */
+add_action( 'wp_enqueue_scripts', 					'storechild_add_customizer_css', 1000 );
+
+/**
  * Customizer default color tweaks
  */
 
