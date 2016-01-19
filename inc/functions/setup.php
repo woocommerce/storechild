b0db4363b2fa5f2e1e8acfb743823419a09218cd
James Koster <james@jameskoster.co.uk>
2016-01-19 09:49:06 +0000

    Add a comment explaining why we conditionally enqueue the stylesheet

--- inc/functions/setup.php
+++ inc/functions/setup.php
@@ -27,7 +27,11 @@ function storechild_enqueue_styles() {
  */
 function storechild_enqueue_child_styles() {
 	global $storefront_version, $storechild_version;
-	
+
+	/**
+	 * Since Storefront 1.5.3 the core codebase automatically loads child theme stylesheets via `storefront_child_scripts()`
+	 * If that function doesn't exist (IE someone is using an old version of Storefront) we'll need to manually enqueue this child themes stylesheet
+	 */
 	if ( ! function_exists( 'storefront_child_scripts' ) ) {
 		wp_enqueue_style( 'storefront-child-style', get_stylesheet_uri(), array( 'storefront-style' ), $storechild_version );
 	}
